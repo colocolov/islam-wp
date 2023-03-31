fed30f8b045574a6a5b0a4980015416dc3862acd
Stas Colocolov <colocolov.stas@gmail.com>
2023-03-31 23:59:09 +0300

    start project

---
+++ wp-content/themes/replyislam/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+// var sass = require('gulp-sass')(require("sass"));
+
+// gulp.task('sass', function(done) {
+//     gulp.src("app/css/sass/*.sass") // путь к sass файлам
+//         .pipe(sass())
+//         .pipe(gulp.dest("app/css/")) //папка для выгрузки css файлов
+//         .pipe(browserSync.stream());
+
+
+//     done();
+// });
+
+gulp.task('serve', function(done) {
+
+    browserSync.init({
+        proxy: 'http://otve-islamu.loc',
+        host: 'otve-islamu.loc',
+        open: 'external',
+    });
+
+    // gulp.watch("app/css/*.sass", gulp.series('sass'));
+    gulp.watch("*.php").on('change', () => {
+        browserSync.reload();
+        done();
+    });
+
+    done();
+});
+
+gulp.task('default', gulp.series('serve'));
